a589c484c72835997c1ea120117f502c74f5d50f
Richard Hallett <richard@richardhallett.com>
2023-01-11 16:01:46 +0100

    nvim - Install vim treesitter by default

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -122,7 +122,7 @@ pcall(require('telescope').load_extension, 'fzf')
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = {'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help'},
+    ensure_installed = {'c', 'cpp', 'go', 'lua', 'vim', 'python', 'rust', 'typescript', 'help'},
 
     highlight = {
         enable = true

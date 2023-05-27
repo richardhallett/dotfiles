2c175fb888fd50875f4b2031a74cad35ad811efc
Richard Hallett <richard@richardhallett.com>
2023-05-27 09:15:02 +0100

    Improve rust config

--- config/nvim/lua/plugins/lang/rust.lua
+++ config/nvim/lua/plugins/lang/rust.lua
@@ -8,23 +8,47 @@ return {
             servers = {
                 rust_analyzer = {
                     mason = false,
-                    tools = {
-                        runnables = {
-                            use_telescope = true
-                        },
-                        inlay_hints = {
-                            auto = true,
-                            show_parameter_hints = true,
-                        },
-                    },
                 }
             },
             setup = {
                 rust_analyzer = function(_, opts)
-                    require("rust-tools").setup(opts)
+                    local rt = require("rust-tools")
+                    rt.setup({
+                        server = {
+                            on_attach = function(_, bufnr)
+                                -- Hover actions
+                                vim.keymap.set("n", "<C-Space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                                -- Code action groups
+                                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                            end,
+                            settings = {
+                                ["rust-analyzer"] = {
+                                    checkOnSave = {
+                                        command = "clippy"
+                                    },
+                                }
+                            }
+                        },
+                        tools = {
+                            inlay_hints = {
+                                auto = true,
+                                show_parameter_hints = true,
+                            },
+                        },
+                    })
                 end
             }
         }
-    }
+    },
+
+    {
+        'saecki/crates.nvim',
+        ft = { "rust", "toml" },
+        config = function(_, opts)
+            local crates = require("crates")
+            crates.setup(opts)
+            crates.show()
+        end
+    },
 
 }

acf60c3d9e80e8c7bb92c031eba364ca3dc3e011
Richard Hallett <richard@richardhallett.com>
2023-05-28 09:23:01 +0100

    Add godot specific dap settings

---
+++ config/nvim/lua/plugins/lang/godot.lua
@@ -0,0 +1,26 @@
+return {
+
+    -- Godot dap config
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require('dap')
+            dap.adapters.godot = {
+              type = "server",
+              host = '127.0.0.1',
+              port = 6006,
+            }
+
+            dap.configurations.gdscript = {
+                {
+                  type = "godot",
+                  request = "launch",
+                  name = "Launch scene",
+                  project = "${workspaceFolder}",
+                  launch_scene = true,
+                }
+            }
+        end,
+    },
+
+}

bb6a9df5e8b72a870b29719b7bc2567bc8187088
Richard Hallett <richard@richardhallett.com>
2023-12-12 15:30:36 +0100

    Add find buffers telescope

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -73,6 +73,7 @@ return {
             }) end, desc = "Search in current buffer" },
             { "<leader>fg", function() require('telescope.builtin').live_grep() end, desc = "Find in Files (Grep)" },
             { "<leader>ff", function() require('telescope.builtin').find_files() end, desc = "Find in Files (cwd)" },
+            { "<leader>fb", function() require('telescope.builtin').buffers() end, desc = "Find in Buffers" },
             { "<leader>fw", function() require('telescope.builtin').grep_string() end, desc = "Find current word" },
             { "<leader>fh", function() require('telescope.builtin').help_tags() end, desc = "Find in Help" },
             { "<leader>fd", function() require('telescope.builtin').diagnostics() end, desc = "Find in Diagnostics" },

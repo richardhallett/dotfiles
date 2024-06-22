23f45b60bcaefa55d7da59e19d914cc351d3d5c4
Richard Hallett <richard@richardhallett.com>
2024-06-22 16:16:05 +0200

    Change find in files to have fallback to git find

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -78,7 +78,28 @@ return {
                 desc = "Search in current buffer"
             },
             { "<leader>fg", function() require('telescope.builtin').live_grep() end,                    desc = "Find in Files (Grep)" },
-            { "<leader>ff", function() require('telescope.builtin').find_files() end,                   desc = "Find Files (cwd)" },
+            {
+                "<leader>ff",
+                function()
+                    local function is_git_repo()
+                        vim.fn.system("git rev-parse --is-inside-work-tree")
+                        return vim.v.shell_error == 0
+                    end
+                    local function get_git_root()
+                        local dot_git_path = vim.fn.finddir(".git", ".;")
+                        return vim.fn.fnamemodify(dot_git_path, ":h")
+                    end
+                    local opts = {}
+                    if is_git_repo() then
+                        opts = {
+                            cwd = get_git_root(),
+                        }
+                    end
+                    require("telescope.builtin").find_files(opts)
+                end,
+                desc = "Find Files (cwd)"
+            },
+            -- { "<leader>ff", function() require('telescope.builtin').find_files() end,                   desc = "Find Files (cwd)" },
             { "<leader>fb", function() require('telescope.builtin').buffers() end,                      desc = "Find Buffers" },
             { "<leader>fw", function() require('telescope.builtin').grep_string() end,                  desc = "Find current word" },
             { "<leader>fh", function() require('telescope.builtin').help_tags() end,                    desc = "Find in Help" },

9e766fb8f6891ba8855cd67fb4541e943f6391ba
Richard Hallett <richard@richardhallett.com>
2024-07-03 12:15:33 +0200

    Add optional switch for nvim in vscode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,18 +1,22 @@
 require('opts')
 require('keys')
 
--- Eagerly disable netrw before we do anything
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+if vim.g.vscode then
 
--- Bootstrap lazy plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+else
+    -- Eagerly disable netrw before we do anything
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    -- Bootstrap lazy plugin manager
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system(
+            { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+                lazypath })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    -- Load plugins specs in plugins module i.e ./plugins
+    require("lazy").setup({ { import = "plugins" }, { import = "plugins.lang" } })
 end
-vim.opt.rtp:prepend(lazypath)
-
--- Load plugins specs in plugins module i.e ./plugins
-require("lazy").setup({{import = "plugins"}, {import = "plugins.lang"}})

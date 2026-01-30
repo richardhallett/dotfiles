3430e7dab585c8e715ce2541ce42505ee2ab6258
Richard Hallett <richard@richardhallett.com>
2026-01-30 20:14:09 +0100

    Lazyvim based vim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,2 @@
-require('opts')
-require('keys')
-
-if vim.g.vscode then
-
-else
-    -- Eagerly disable netrw before we do anything
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    -- Bootstrap lazy plugin manager
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system(
-            { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-                lazypath })
-    end
-    vim.opt.rtp:prepend(lazypath)
-
-    -- Load plugins specs in plugins module i.e ./plugins
-    require("lazy").setup({ { import = "plugins" }, { import = "plugins.lang" } })
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,90 +1,10 @@
--- Constant that defines UI buffers that we do general exclusion of plugins
-local UI_BUFFERS = { "help", "alpha", "NvimTree", "Trouble", "lazy", "netrw", "text" }
-
 return {
-
-    -- Status line
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-            theme = "gruvbox_dark",
-            extensions = {},
-        }
-    },
-
-    -- Bufferline
-    {
-        "akinsho/nvim-bufferline.lua",
-        event = "VeryLazy",
-        init = function()
-            vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
-            vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
-        end,
-        opts = {
-            options = {
-                diagnostics = "nvim_lsp",
-                always_show_bufferline = false,
-                mode = "tabs",
-            },
-        },
-    },
-
-    -- Automatic switching for relative line numbers when in different modes
-    {
-        "sitiom/nvim-numbertoggle",
-        event = "BufReadPre",
-    },
-
-    -- Add visible indentation
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = "BufReadPre",
-        main = "ibl",
-        opts = {
-            indent = {
-                char = '┊',
-                tab_char = '┊',
-            },
-            scope = {
-                enabled = false,
-            },
-            exclude = {
-                filetypes = UI_BUFFERS,
-                buftypes = { "terminal", "nofile" },
-            },
-        },
-    },
-
-    -- Additional UI for vim.ui.select i.e. to popup telescope
-    {
-        "stevearc/dressing.nvim",
-        opts = {
-            dressing = {
-                border = {
-                    style = "rounded",
-                    highlight = "Normal",
-                },
-                prompt = {
-                    highlight = "Comment",
-                },
-                results = {
-                    highlight = "Normal",
-                },
-                preview = {
-                    highlight = "Normal",
-                },
-            },
-        },
-    },
-
-    -- Dashboard
-    {
-        "goolord/alpha-nvim",
-        event = "VimEnter",
-        opts = function()
-            local dashboard = require("alpha.themes.dashboard")
-            local logo = [[
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
             +----------------------------+
             |                            |
             |            /\        /\    |
@@ -102,30 +22,9 @@ return {
             |               \/           |
             |                            |
             +-----------------------------
-        ]]
-
-            dashboard.section.header.val = vim.split(logo, "\n")
-            dashboard.section.buttons.val = {
-                dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-                dashboard.button("n", " " .. " New file", ":ene <CR>"),
-                dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-                dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-                dashboard.button("s", "󱉟" .. " Restore Session", ":NeovimProjectLoadRecent<CR>"),
-                dashboard.button("l", "l " .. " Load Session", ":Telescope neovim-project discover<CR>"),
-                dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-            }
-            for _, button in ipairs(dashboard.section.buttons.val) do
-                button.opts.hl = "AlphaButtons"
-                button.opts.hl_shortcut = "AlphaShortcut"
-            end
-            dashboard.section.footer.opts.hl = "Type"
-            dashboard.section.header.opts.hl = "AlphaHeader"
-            dashboard.section.buttons.opts.hl = "AlphaButtons"
-            dashboard.opts.layout[1].val = 8
-            return dashboard
-        end,
-        config = function(_, dashboard)
-            require("alpha").setup(dashboard.opts)
-        end,
+          ]],
+        },
+      },
     },
+  },
 }
